7b0e8e19c64c853807eb894bdc8bc6e784526383
lolkaenot <lolkaenot@gmail.com>
2016-02-26 23:43:51 +0300

    тест вход

---
+++ www/index.php
@@ -0,0 +1,12 @@
+<?php
+error_reporting(E_ALL);
+require __DIR__ . '/autoload.php';
+
+$cont = isset($_GET['cont']) ? $_GET['cont'] : 'News';
+$act = isset($_GET['act']) ? $_GET['act'] : 'All';
+
+
+$contName = 'App\\Controllers\\' . $cont;
+$controller = new $contName;
+$method = 'action' . $act;
+$controller->$method();
