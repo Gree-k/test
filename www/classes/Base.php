fbb200df1c0a5e2c7c842a429613367fd9714956
lolkaenot <lolkaenot@gmail.com>
2016-02-26 18:14:55 +0300

    описал класс для работы с бд

--- www/classes/Base.php
+++ www/classes/Base.php
@@ -1,12 +1,29 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: YOBA
- * Date: 26.02.2016
- * Time: 14:05
- */
 class Base
 {
+    protected $dbn;
+    protected $className = 'stdClass';
 
+    public function __construct() {
+        $this->dbn = new PDO('mysql:dbname=test;host=localhost', 'root', '');
+    }
+
+    public function setClassName($name) {
+        $this->className = $name;
+    }
+
+    public function sql_query($str, $param=[]){
+        $sth = $this->dbn->prepare($str);
+        $sth->execute($param);
+        return $sth->fetchAll(PDO::FETCH_CLASS, $this->className);
+
+    }
+
+    public function sql_execute($str, $param=[]){
+        $sth = $this->dbn->prepare($str);
+        return $sth->execute($param);
+
+
+    }
 }
