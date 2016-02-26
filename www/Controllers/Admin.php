3e9dd00e8a88580514228b1ba3cd25dbb61ccb90
lolkaenot <lolkaenot@gmail.com>
2016-02-26 21:11:31 +0300

    Класс для Администратора

---
+++ www/Controllers/Admin.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Controllers;
+
+class Admin {
+    public function actionDel() {
+        $id = $_GET['id'];
+        $n = News::getOneById($id);
+        $n->delete();
+        View::mainPage();
+
+    }
+
+    public function actionSave() {
+        if (!empty($_POST['title']) && !empty($_POST['text'])) {
+            $new = new News();
+            foreach ($_POST as $key => $value) {
+                $new->$key = $value;
+            }
+            $new->date=date("Y.m.d H:i:s");
+            $new->save();
+            View::mainPage();
+        }
+    }
+
+    public function actionForm()
+    {
+        $view = new View();
+        if (isset($_GET['id'])) {
+            $item = News::getOneById($_GET['id']);
+            $view->news = $item;
+        }
+        $view->display('news/form.php');
+    }
+
+}
