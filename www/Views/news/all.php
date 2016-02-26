97425e13e5a4d9145c7cd1cbe7078e4101e9884b
lolkaenot <lolkaenot@gmail.com>
2016-02-26 23:38:24 +0300

    вывод пользователю

---
+++ www/Views/news/all.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<div>
+    <?php foreach ($news as $new):?>
+
+            <a href="index.php?act=One&id=<?=$new->id;?>"><h1><?= $new->title; ?></h1></a><br>
+            <p><?= substr($new->text,0,150)?>
+                <?php if(strlen($new->text)>150):?>
+                    &nbsp;...&nbsp;
+                    <a href="index.php?act=One&id=<?=$new->id; ?>">>>>&nbsp;Читать далее</a>
+                <?php endif;?>
+            </p>
+            <p style="font-size: 12px; margin: 0; padding: 0;"><?=$new->date;?></p>
+            <a href="index.php?act=Form&id=<?=$new->id;?>">Редактировать</a>
+            <a href="index.php?act=Del&id=<?=$new->id;?>">Удалить</a><br>
+    <?php endforeach;?>
+    <p><a href="index.php?act=Form">Добавить статью</a></p>
+</div>
+</body>
+</html>
---
+++ www/Views/news/one.php
@@ -0,0 +1,3 @@
+<p><a href="../../index.php">&larr;Назад</a></p>
+<h1><?= $news->title;?></h1>
+<p><?=$news->text;?></p>
