277a089668d95a2b9b41f9e7238e2fe84a3745ad
Gree-k <alexandr.greek@mail.ru>
2016-02-28 03:48:09 +0300

    переделан вывод на экран всех новостей в соответсвии с шаблоном, новым получением данных из бд, а так же добавленно место для будуещего предложения о регистрции (под ?)

--- www/Views/news/all.php
+++ www/Views/news/all.php
@@ -1,16 +1,22 @@
+<div class="col-sm-8 blog-main">
 <?php foreach ($news as $new):?>
-    <div>
-        <a href="index.php?act=One&id=<?=$new->id;?>"><h1><?= $new->title; ?></h1></a><br>
-        <p><?= mb_substr($new->text,0,150)?>
-            <?php if(strlen($new->text)>150):?>
+    <div class="blog-post">
+        <a href="index.php?act=One&id=<?=$new->id;?>"><h2 class="blog-post-title"><?= $new->title; ?></h2></a><br>
+        <p class="blog-post-meta"><?=$new->date;?> by <?=$new->login;?></p>
+        <p><?= mb_substr($new->text,0,500)?>
+            <?php if(mb_strlen($new->text)>500):?>
                 &nbsp;&hellip;&nbsp;
-                <a href="index.php?act=One&id=<?=$new->id; ?>">Читать далее</a>
+                <a href="index.php?act=One&id=<?=$new->id; ?>">Читать&nbsp;далее</a>
             <?php endif;?>
         </p>
-        <p><em>Опубликованно: <?=$new->date;?></em></p>
     </div>
 
-    <!--            <a href="index.php?act=Form&id=--><?//=$new->id;?><!--">Редактировать</a>-->
-    <!--            <a href="index.php?act=Del&id=--><?//=$new->id;?><!--">Удалить</a><br>-->
 <?php endforeach;?>
-    <!--    <p><a href="index.php?act=Form">Добавить статью</a></p>-->
+</div>
+
+<div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+    <div class="sidebar-module sidebar-module-inset">
+        <h4>About</h4>
+        <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+    </div>
+</div>
