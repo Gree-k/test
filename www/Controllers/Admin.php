bd9af7ad06c29cf26b93759da7dc320994c5473b
Gree-k <alexandr.greek@mail.ru>
2016-02-29 09:19:54 +0300

    подправленны мелочи + класс показывющий все новости в админ панеле

--- www/Controllers/Admin.php
+++ www/Controllers/Admin.php
@@ -3,36 +3,46 @@ namespace App\Controllers;
 
 use App\Classes\View;
 use App\Models\News;
+use App\Models\User;
 
 class Admin {
-    public function actionDel() {
+    public function actionDeleteNews() {
         $id = $_GET['id'];
         $n = News::getOneById($id);
         $n->delete();
-        View::mainPage();
+        View::adminPanel();
 
     }
 
-    public function actionSave() {
+    public function actionSaveNews() {
         if (!empty($_POST['title']) && !empty($_POST['text'])) {
             $new = new News();
             foreach ($_POST as $key => $value) {
                 $new->$key = $value;
             }
+            $user = User::getUserByUserName($_SESSION['username']);
+            $new->user_id = $user->id;
             $new->date=date("Y.m.d H:i:s");
             $new->save();
-            View::mainPage();
+            View::adminPanel();
         }
     }
 
-    public function actionForm()
+    public function actionFormNews()
     {
         $view = new View();
         if (isset($_GET['id'])) {
             $item = News::getOneById($_GET['id']);
             $view->news = $item;
         }
-        $view->display('News/form.php');
+        $view->displayAdmin('News/form.php');
     }
 
+    public function actionAll() {
+        $items = News::getAllNewsAndAuthorReverseSort('date');
+        $view = new View();
+        $view->news = $items;
+        $view->displayAdmin('News/all.php');
+
+    }
 }
