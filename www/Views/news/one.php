177533a394f6c07f2583294557b6d56545931308
Gree-k <alexandr.greek@mail.ru>
2016-02-29 03:56:19 +0300

    обернул статью в див

--- www/Views/news/one.php
+++ www/Views/news/one.php
@@ -1,3 +1,5 @@
-<p><a href="../../index.php">&larr;Назад</a></p>
-<h1><?= $news->title;?></h1>
-<p><?=str_replace('<br />', '</p><p>', nl2br($news->text));?></p>
+<p><a href="/index.php">&larr;Назад</a></p>
+<div class="blog-post">
+    <h1><?= $news->title; ?></h1>
+    <p><?= str_replace('<br />', '</p><p>', nl2br($news->text)); ?></p>
+</div>
