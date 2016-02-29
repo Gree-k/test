d25552672d7343f6a5ab6e8db9bfb586787f01bc
Gree-k <alexandr.greek@mail.ru>
2016-02-29 03:59:11 +0300

    поведения страницы при авторизации пользователя

--- www/Views/template.php
+++ www/Views/template.php
@@ -5,8 +5,10 @@
     <title>Title</title>
 
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/Views/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="/JavaScripts/bootstrap.js"></script>
+    <link rel="stylesheet" href="/Views/style.css">
+    <script src="/JavaScripts/Authorization.js"></script>
 </head>
 <body>
 
@@ -22,32 +24,41 @@
         </div>
         <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Новости</a></li>
-                <li><a href="#about">Галерея</a></li>
-                <li><a href="#contact">Змейка</a></li>
+                <li class="active"><a href="index.php">Новости</a></li>
+                <li><a href="#">Галерея</a></li>
+                <li><a href="#">Змейка</a></li>
             </ul>
             <div class="navbar-right" style="padding-top: 7px;">
-                <div class="btn-group">
-                    <button type="button" class="btn btn-default">Вход</button>
-                    <button type="button" class="btn btn-default">Регистрация</button>
-                </div>
+                <?php if (isset($_SESSION['username'])):?>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-default" data-toggle="dropdown">
+                            <?=$_SESSION['username'];?> <span class="caret"></span></button>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Например ЛК</a></li>
+                            <li><a href="#">Если Админ то Админ панель</a></li>
+                            <li><a href="#">Что-то иное</a></li>
+                            <li class="divider"></li>
+                            <li><a href="/index.php?cont=Site&act=Logout">Выход</a></li>
+                        </ul>
+                        <a class="btn btn-default" href="/index.php?cont=Site&act=Logout">
+                            <span class="glyphicon glyphicon-log-out"></span></a>
+                    </div>
+                <?php else: ?>
+                    <button type="button" id="login" class="btn btn-default" style="padding-left: 7px;"><span
+                            class="glyphicon glyphicon-log-in"></span>&nbsp;&nbsp;Вход
+                    </button>
+                <?php endif; ?>
             </div>
         </div>
     </div>
 </div>
 
 <div class="container">
-
-    <div class="blog-header">
-
-        <p class="lead blog-description">Тестовый вариант новостной ленты</p>
-    </div>
-
     <div class="row">
-            <?php include __DIR__ . '/' . $file ;?>
+        <?php include __DIR__ . '/' . $file; ?>
     </div>
-
 </div>
+
 
 
 <div id="footer">
@@ -55,35 +66,22 @@
         <p class="text-muted">&copy; made by <a href="#">Greek</a></p>
     </div>
 </div>
-<!--<div class="blog-footer">-->
-<!--    <p>made by <a href="#">Greek</a></p>-->
-<!--    <p>-->
-<!--        <a href="#">Back to top</a>-->
-<!--    </p>-->
-<!--</div>-->
 
 
-<!--<button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">-->
-<!--    Посмотреть демо-->
-<!--</button>-->
-
-<!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div id="loginModal" class="modal fade ">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title" id="myModalLabel">Название модали</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h4 class="modal-title">Авторизация</h4>
             </div>
-            <div class="modal-body">
-                ...
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
-                <button type="button" class="btn btn-primary">Сохранить изменения</button>
+            <div class="modal-body" id="modalFormLogin">
+                <!--форма авторизации-->
             </div>
         </div>
     </div>
 </div>
+
+
 </body>
 </html>
