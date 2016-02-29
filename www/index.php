88a00857e5e007a997e1c217d5fee2753960775d
Gree-k <alexandr.greek@mail.ru>
2016-02-29 09:15:44 +0300

    проверка сессий и кук, если надо то установка

--- www/index.php
+++ www/index.php
@@ -3,6 +3,13 @@ session_start();
 error_reporting(E_ALL);
 require __DIR__ . '/autoload.php';
 
+if (isset($_COOKIE['username']) && !isset($_SESSION['username'])) {
+    \App\Controllers\Site::setSessionByCookie();
+}
+if (isset($_SESSION['username']) && !isset($_SESSION['access'])) {
+    \App\Controllers\Site::setSessionAccess();
+}
+
 $cont = isset($_GET['cont']) ? $_GET['cont'] : 'News';
 $act = isset($_GET['act']) ? $_GET['act'] : 'All';
 
