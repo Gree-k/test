09d826b589dfbdbc6dd20a2955e3b3aacaa42d2f
Gree-k <alexandr.greek@mail.ru>
2016-02-29 09:14:05 +0300

    шаблон админ панели

--- www/Views/Admin/template.php
+++ www/Views/Admin/template.php
@@ -1,7 +1,61 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: YOBA
- * Date: 29.02.2016
- * Time: 5:03
- */
+if (empty($_SESSION['access']) || 'admin' != $_SESSION['access']) {
+    \App\Classes\View::mainPage();
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="/Views/Admin/adminStyle.css">
+    <!--    <script src="/JavaScripts/bootstrap.js"></script>-->
+</head>
+<body>
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Панель администратора</a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="index.php">На сайт</a></li>
+                <li><a href="#">Settings</a></li>
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">Help</a></li>
+            </ul>
+            <form class="navbar-form navbar-right">
+                <input type="text" class="form-control" placeholder="Search...">
+            </form>
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+            <ul class="nav nav-sidebar">
+                <li class="active"><a href="#">Новости</a></li>
+                <li><a href="#">Галерея</a></li>
+                <li><a href="#">Пользователи</a></li>
+                <li><a href="#">Коментарии</a></li>
+            </ul>
+        </div>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+
+            <?php include __DIR__ . '/' . $file;?>
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
