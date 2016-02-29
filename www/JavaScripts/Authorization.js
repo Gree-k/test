6cd4854027b54439d7a17ecfd28ac8dd0636e758
Gree-k <alexandr.greek@mail.ru>
2016-02-29 09:17:22 +0300

    autofocus и require у инпутов

--- www/JavaScripts/Authorization.js
+++ www/JavaScripts/Authorization.js
@@ -7,8 +7,8 @@ $(function () {
         e.preventDefault();
         authorization();
     });
-
 });
+
 $(function(){
     $('body').on('click','#registration' ,function(e){
         e.preventDefault();
@@ -17,33 +17,32 @@ $(function(){
             '<form action="/index.php?cont=Site&act=Registration" method="post">' +
             '<table id="tableForm">' +
             '<tr><th>Имя</th>' +
-            '<td><input type="text"  class="form-control" name="name"></td></tr>' +
+            '<td><input type="text"  class="form-control" name="name" autofocus required></td></tr>' +
             '<tr><th>Фамилия</th>' +
-            '<td><input type="text"  class="form-control" name="surname"></td></tr>' +
+            '<td><input type="text"  class="form-control" name="surname" required></td></tr>' +
             '<tr><th>Логин</th>' +
-            '<td><input type="text"  class="form-control" name="username"></td></tr>' +
+            '<td><input type="text"  class="form-control" name="username" required></td></tr>' +
             '<tr><th>Пароль</th>' +
-            '<td><input type="password"  class="form-control" name="password"></td></tr>' +
+            '<td><input type="password"  class="form-control" name="password" required></td></tr>' +
             '<tr><th rowspan="2"></th>' +
             '<td><a id="authorization" href="#" class="pull-right">Авторизция</a></tr>' +
             '<tr><td><input type="submit" class="btn btn-primary pull-right"  value="Регистрация" ></td></tr>' +
             '</table></form>');
-
     })
 });
+
 function authorization() {
     $('.modal-title').text('Авторизация');
     $('#modalFormLogin').empty().append('' +
         '<form action="/index.php?cont=Site&act=Login" method="post">' +
         '<table id="tableForm">' +
         '<tr><th>Логин</th>' +
-        '<td><input type="text"  class="form-control" name="username"></td></tr>' +
+        '<td><input type="text"  class="form-control" name="username" autofocus required></td></tr>' +
         '<tr><th>Пароль</th>' +
-        '<td><input type="password"  class="form-control" name="password"></td></tr>' +
+        '<td><input type="password"  class="form-control" name="password" required></td></tr>' +
         '<tr><th rowspan="2"></th>' +
         '<td><input type="checkbox" name="remember"> Запомнить меня' +
         '<a id="registration" href="#" class="pull-right">Регистрация</a></tr>' +
         '<tr><td><input type="submit" class="btn btn-primary pull-right" value="Войти" ></td></tr>' +
         '</table></form>');
-
 }
