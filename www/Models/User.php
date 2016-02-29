d213a742fd81d130fa9480dde282c839c5e0eb79
Gree-k <alexandr.greek@mail.ru>
2016-02-29 09:11:54 +0300

    класс для работы с пользователями: -получения списка всех -получение информации об одном по его имени

--- www/Models/User.php
+++ www/Models/User.php
@@ -1,15 +1,37 @@
 <?php
 namespace App\Models;
 
+use App\Classes\Base;
+
 /**
  * Class User
+ * @package $id
+ * @package $access
+ * @package $username
  * @package $name
  * @package $surname
- * @package $username
  * @package $password
  * @package $date;
  */
 class User extends AbstractModel {
     protected static $table = 'st_user';
 
+    static public function getAllUsers() {
+        $bd = new Base();
+        $str ='SELECT st_user.id, st_lvlaccess.access, username, name, surname, password, date
+            FROM ' . self::$table . ' LEFT OUTER JOIN st_lvlaccess ON st_user.lvlaccess_id=st_lvlaccess.id';
+        $bd->setClassName(get_called_class());
+        $res = $bd->sql_query($str);
+        return $res;
+    }
+
+    static public function getUserByUserName($username) {
+        $bd = new Base();
+        $bd->setClassName(get_called_class());
+        $str = 'SELECT st_user.id, st_lvlaccess.access, username, name, surname, password, date
+            FROM ' . self::$table . ' LEFT OUTER JOIN st_lvlaccess ON st_user.lvlaccess_id=st_lvlaccess.id
+            WHERE username=:username';
+        $res = $bd->sql_query($str, [':username' => $username]);
+        return $res[0];
+    }
 }
