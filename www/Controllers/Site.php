c1794091da2032ef750a7b76df173705b059eb3a
Gree-k <alexandr.greek@mail.ru>
2016-02-29 03:57:20 +0300

    контроллер по работе с сайтом добавленно: -вход пользователя -выход -регистрация

--- www/Controllers/Site.php
+++ www/Controllers/Site.php
@@ -1,15 +1,46 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: YOBA
- * Date: 29.02.2016
- * Time: 1:33
- */
-
 namespace App\Controllers;
 
+use App\Classes\View;
+use App\Models\User;
 
-class Site
-{
+class Site {
 
+    public function actionLogin() {
+        $user=User::findByColumn('username', $_POST['username']);
+        if (!empty($user) && 1==count($user)) {
+            $user = $user[0];
+            if ($user->password == $_POST['password']) {
+                $_SESSION['username']=$user->username;
+                if (isset($_POST['remember'])) {
+                    setcookie('username', $user->username, time() + 60 * 60 * 24);
+                }
+                View::mainPage();
+            }
+        }
+        //созать показ сообщения о не верном логине иди пароле
+        View::mainPage();
+    }
+
+    public function actionLogout() {
+        unset($_SESSION['username']);
+        if (isset($_COOKIE['username'])) {
+            setcookie('username', '', time() - 60 * 60 * 24);
+        }
+        View::mainPage();
+    }
+
+    public function actionRegistration() {
+        $user = new User();
+        foreach ($_POST as $key => $value) {
+            $user->$key = trim($value);
+        }
+        $user->date=date("Y.m.d H:i:s");
+        $fin = $user->save();
+        if ($fin) {
+            $_SESSION['username']=$user->username;
+        }
+        View::mainPage();
+
+    }
 }
