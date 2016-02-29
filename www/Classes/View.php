927188a69fe4028b6aa8dbd23aae1b748d5e2afd
Gree-k <alexandr.greek@mail.ru>
2016-02-29 09:10:55 +0300

    отображение для админ панели

--- www/Classes/View.php
+++ www/Classes/View.php
@@ -23,7 +23,16 @@ class View {
         include __DIR__ . '/../Views/template.php';
     }
 
+    public function displayAdmin($file) {
+        foreach ($this->data as $key => $value) {
+            $$key = $value;
+        }
+        include __DIR__ . '/../Views/Admin/template.php';
+    }
     public static function mainPage() {
         header('Location: /index.php');
+    }
+    public static function adminPanel() {
+        header('Location: /index.php?cont=Admin');
     }
 }
