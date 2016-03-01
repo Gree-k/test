334dab8b7998871ebd3020275351e1f069f7248d
Gree-k <alexandr.greek@mail.ru>
2016-03-01 08:08:11 +0300

    получение всех коментариев и части данных пользователя по id новости

--- www/Models/Comment.php
+++ www/Models/Comment.php
@@ -1,15 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: YOBA
- * Date: 01.03.2016
- * Time: 6:22
- */
-
 namespace App\Models;
 
+use App\Classes\Base;
 
-class Comment
-{
+class Comment extends AbstractModel {
 
+    protected static $table = 'st_comment';
+
+    static public function getCommentsByNewsPk($newsId) {
+        $bd = new Base();
+        $str ='SELECT st_comment.id, st_comment.comment, st_comment.date, st_user.username, st_user.name, st_user.surname
+            FROM ' . self::$table . ' LEFT OUTER JOIN st_user ON st_comment.user_id=st_user.id
+            WHERE st_comment.articles_id=:id ORDER BY st_comment.date DESC';
+        $bd->setClassName(get_called_class());
+        $res = $bd->sql_query($str, [':id' => $newsId]);
+        return $res;
+    }
 }
