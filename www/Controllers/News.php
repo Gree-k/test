cd8b6201b79d6ef69edfb4aa727d06e7551d7da7
Gree-k <alexandr.greek@mail.ru>
2016-03-01 12:45:36 +0300

    добавление коментария к новости

--- www/Controllers/News.php
+++ www/Controllers/News.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 use App\Classes\View;
 use App\Models\Comment;
 use App\Models\News as NewsModel;
+use App\Models\User;
 
 
 class News {
@@ -23,5 +24,19 @@ class News {
         $view->news = $item;
         $view->comments = $com;
         $view->display('News/one.php');
+    }
+
+    public function actionAddCommentToNews() {
+        if (!empty($_POST['comment'])) {
+            $com = new Comment();
+            foreach ($_POST as $key => $value) {
+                $com->$key = $value;
+            }
+            $user = User::getUserByUsername($_SESSION['username']);
+            $com->user_id = $user->id;
+            $com->date=date("Y.m.d H:i:s");
+            $com->add();
+            header('Location: /index.php?cont=News&act=One&id=' . $_POST['articles_id'] . '#commentAnchor');
+        }
     }
 }
