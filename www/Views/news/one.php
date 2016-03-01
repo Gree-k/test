4f2a11a48afcddca4568b4f37ccad20492ccc3b5
Gree-k <alexandr.greek@mail.ru>
2016-03-01 08:10:20 +0300

    отображение коментариев на странице одной новости

--- www/Views/news/one.php
+++ www/Views/news/one.php
@@ -3,3 +3,37 @@
     <h1><?= $news->title; ?></h1>
     <p><?= str_replace('<br />', '</p><p>', nl2br($news->text)); ?></p>
 </div>
+<div class="sub-header">
+    <h4 id="commentAnchor"><span class="glyphicon glyphicon-comment"></span> Комментарии</h4>
+</div>
+<div id="formComment">
+    <form>
+        <label  style="width: 100%">
+            <textarea class="form-control" name="text" rows="3" maxlength="255" style="resize: none;"></textarea>
+        </label>
+        <div class="text-right">
+            <button type="submit" class="btn btn-primary ">Отправить</button>
+        </div>
+    </form>
+</div>
+<?php foreach ($comments as $com):?>
+
+    <div class="comment">
+        <table>
+            <tr>
+                <td rowspan="2">
+                    //сделать в бд модель с аватаром и выводить сюда
+                    <img src="/Views/Style/non-profile.jpg" class="pull-left ">
+                </td>
+                <td class="sub-header">
+                    <h4 class="pull-left" style="color: lightcoral;"><?=$com->name . ' ' . $com->surname;?> </h4>
+                    <p class="pull-right"><?=$com->date;?></p>
+                </td>
+            </tr>
+            <tr>
+                <td><p style="font-size: 16px"><?=$com->comment;?></p></td>
+            </tr>
+        </table>
+    </div>
+
+<?php endforeach;?>
