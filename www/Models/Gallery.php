86c6f8462b2bb7491a3cd9cde872b0a36637d112
Gree-k <alexandr.greek@mail.ru>
2016-03-02 07:20:05 +0300

    модель для работы с бд галереи -получение всех записей -проверка на существование имени файла

--- www/Models/Gallery.php
+++ www/Models/Gallery.php
@@ -1,15 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: YOBA
- * Date: 01.03.2016
- * Time: 14:33
- */
-
 namespace App\Models;
 
+use App\Classes\Base;
 
-class Gallery
-{
+/** Реализовать обрезку изображения до квадрата и сжатие + счетчик лайков*/
+
+class Gallery extends AbstractModel{
+    protected static $table = 'st_gallery';
+
+    static public function getAllImg(){
+        $bd = new Base();
+        $str= 'SELECT * FROM ' . static::$table. ' ORDER BY id DESC';
+        $bd->setClassName(get_called_class());
+        return $bd->sql_query($str);
+    }
+
+    static public function haveName($newName) {
+        $bd = new Base();
+        $str= 'SELECT EXISTS (SELECT * FROM ' . static::$table . ' WHERE name=:name)';
+        $bd->setClassName(get_called_class());
+        return $bd->sql_queryFetch($str,[':name' => $newName])[0];
+
+    }
+
+
 
 }
