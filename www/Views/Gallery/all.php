dcf3099d0154f412a47360e997dc51e5f47fbc9a
Gree-k <alexandr.greek@mail.ru>
2016-03-02 07:15:51 +0300

    показ все изображений

--- www/Views/Gallery/all.php
+++ www/Views/Gallery/all.php
@@ -1,7 +1,18 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: YOBA
- * Date: 01.03.2016
- * Time: 14:17
- */
+<div <?php echo isset($_SESSION['username']) ? 'class="col-sm-9"' : 'class="col-sm-12"';?> >
+
+    <?php foreach($image as $img):?>
+        <div <?php echo isset($_SESSION['username']) ? 'class="col-xs-4 col-md-4""' : 'class="col-xs-4 col-md-3""';?>>
+            <a href="<?=$img->url;?>" class="thumbnail" target="_blank">
+                <img  alt="<?=$img->name;?>" src="<?=$img->url;?>">
+            </a>
+        </div>
+    <? endforeach;?>
+</div>
+<?php if (isset($_SESSION['username'])): ?>
+    <div class=" col-sm-3  ">
+    <form enctype="multipart/form-data" action="index.php?cont=Gallery&act=SaveImg" method="post">
+        <input type="file" accept="image/jpeg" name="uploadImg" style="margin-bottom: 10px">
+        <button type="submit" class="btn btn-default">Загрузить</button>
+    </form>
+    </div>
+<?php endif ?>
