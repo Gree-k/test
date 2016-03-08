79823c804254e64546b43dd5f931017b5ce64931
Gree-k <alexandr.greek@mail.ru>
2016-03-08 15:12:23 +0300

    скрипт по обработке мод. окна с изменением имени

--- www/JavaScripts/RenameImg.js
+++ www/JavaScripts/RenameImg.js
@@ -1,3 +1,46 @@
-/**
- * Created by YOBA on 07.03.2016.
- */
+$(document).ready(function(){
+    var modalRename = $('#modalRename');
+    var input = $('#newName');
+
+    $('body').on('click','.rename' ,function(e){
+        e.preventDefault();
+        //var modalRename = $('#modalRename');
+        //$('.modal-title').text($(this).data('name'));
+        $('#modalFormRename').attr('action', $(this).attr('href'));
+        input.val($(this).data('name'));
+        modalRename.removeClass('has-error');
+        modalRename.removeClass('has-success');
+        modalRename.modal('show');
+    })
+
+    $('body').on('keyup','#newName' ,function(){
+        if(input.val().trim()==0) {
+            modalRename.addClass('has-error');
+        }else {
+            $.post('/index.php?cont=Site&act=TestName',
+                {
+                    name: input.val().trim()
+                }
+            )
+                .done(function(data) {
+                    if (data != 0) {
+                        modalRename.addClass('has-error');
+                    } else {
+                        if(modalRename.hasClass('has-error')) {
+                            modalRename.removeClass('has-error').addClass('has-success');
+                        }else {
+                            modalRename.addClass('has-success');
+                        }
+                    }
+                })
+        }
+    })
+
+    $('#sendName').click(function(e){
+        if(modalRename.hasClass('has-error') || input.val().trim()==0){
+            e.preventDefault();
+        }else {
+            $(this).submit();
+        }
+    })
+});
