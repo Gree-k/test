5eaffaae73918225be596991db7683ea5fe8aa73
Gree-k <alexandr.greek@mail.ru>
2016-03-08 15:06:00 +0300

    нумерация страниц

--- www/Views/Gallery/all.php
+++ www/Views/Gallery/all.php
@@ -1,3 +1,7 @@
+<div class="blog-header">
+    <p class="lead blog-description">Тестовый вариант галереи</p>
+</div>
+
 <div <?php echo isset($_SESSION['username']) ? 'class="col-sm-9"' : 'class="col-sm-12"';?> >
 
     <?php foreach($image as $img):?>
@@ -7,6 +11,33 @@
             </a>
         </div>
     <? endforeach;?>
+
+    <?php if($count>IMG_ON_PAGE):?>
+        <div class="col-xs-12">
+            <div style="text-align: center;" >
+                <ul class="pagination ">
+                    <?php if(!isset($_GET['page']) || $_GET['page']==1):?>
+                        <li><span>&laquo;</span></li>
+                    <?php else:?>
+                        <li ><a  href="/index.php?cont=Gallery&act=All&page=<?=$_GET['page']-1;?>">&laquo;</a></li>
+                    <?php endif;?>
+
+                    <?for($i=1;$i<=ceil($count/IMG_ON_PAGE);$i++):?>
+                        <li <?php if((!isset($_GET['page']) && $i==1) || (isset($_GET['page']) && $i == $_GET['page'])):?>
+                            class="active"
+                        <?php endif;?>
+                        ><a href="/index.php?cont=Gallery&act=All&page=<?=$i;?>"><?=$i;?></a></li>
+                    <?endfor;?>
+
+                    <?php if(isset($_GET['page']) && ceil($count/IMG_ON_PAGE) == $_GET['page']):?>
+                        <li><span>&raquo;</span></li>
+                    <?php else:?>
+                        <li ><a  href="/index.php?cont=Gallery&act=All&page=<?=$_GET['page']+1;?>">&raquo;</a></li>
+                    <?php endif;?>
+                </ul>
+            </div>
+        </div>
+    <? endif;?>
 </div>
 <?php if (isset($_SESSION['username'])): ?>
     <div class=" col-sm-3 col-xs-4 ">
@@ -16,3 +47,6 @@
     </form>
     </div>
 <?php endif ?>
+
+
+
