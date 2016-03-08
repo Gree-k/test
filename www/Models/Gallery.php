a4a0ac0f00cb63a5885123777b21864331b38e95
Gree-k <alexandr.greek@mail.ru>
2016-03-08 15:09:42 +0300

    - getAll и getLast перенес в абстрактную модель

--- www/Models/Gallery.php
+++ www/Models/Gallery.php
@@ -8,12 +8,6 @@ use App\Classes\Base;
 class Gallery extends AbstractModel{
     protected static $table = 'st_gallery';
 
-    static public function getAllImg(){
-        $bd = new Base();
-        $str= 'SELECT * FROM ' . static::$table. ' ORDER BY id DESC';
-        $bd->setClassName(get_called_class());
-        return $bd->sql_query($str);
-    }
 
     static public function getImgById($id) {
         $bd = new Base();
@@ -25,13 +19,4 @@ class Gallery extends AbstractModel{
         return $res[0];
     }
 
-    static public function getLastImg($start, $limit) {
-        $bd = new Base();
-        $bd->setClassName(get_called_class());
-        $str='SELECT st_gallery.*, st_user.username FROM ' . static::$table . '
-          LEFT OUTER JOIN st_user ON st_gallery.user_id=st_user.id
-          ORDER BY id DESC LIMIT ' . $start . ', ' . $limit;
-        return $bd->sql_query($str);
-
-    }
 }
